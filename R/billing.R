9e99c8a788a7da48d61757f78d48e37e6fda7cae
Scott Chamberlain <sckott@fastmail.com>
2023-10-05 16:33:47 -0700

    various fixes to improve package:
    - reduce code duplication in billing; make a single high level fxn
    - comment out code in bucket.R for now so pkg level stuff can be run
    - users.R: define username param so r check doesnt complain
    - add man files
    - add roxyglobals for the global vars used in dplyr verbs that check complains about
    - add pkg level manual file
    - add a few package imports

--- R/billing.R
+++ R/billing.R
@@ -1,47 +1,49 @@
-library(tidyverse)
-library(paws)
-
+#' Fetch billing data
 #'
-
-ce <- paws::costexplorer()
-
-raw_billing_data <- ce$get_cost_and_usage(
-  TimePeriod = list(Start = "2023-01-01", End = as.character(Sys.Date())),
-  Granularity = "DAILY",
-  Metrics = "UnblendedCost",
-  GroupBy = list(
-    list(Type = "DIMENSION", Key = "SERVICE"),
-    list(Type = "DIMENSION", Key = "LINKED_ACCOUNT")
+#' @importFrom tibble tibble
+#' @importFrom purrr map map_chr list_rbind
+#' @importFrom rlang :=
+#' @param date_start,date_end Start and end date to get billing data for.
+#' Date format expected: `YYYY-MM-DD`
+#' @examples \dontrun{
+#' billing(date_start="2023-01-01")
+#' }
+billing <- function(date_start, date_end = as.character(Sys.Date())) {
+  # TODO: assertions on date formats, and possibly max date - check to
+  # see if paws does any date validation first
+  list(
+    unblended = billing_unblended(date_start, date_end),
+    blended = billing_blended(date_start, date_end)
   )
-)
+}
 
-billing_data <- raw_billing_data$ResultsByTime |>
-  map(function(x){
-    tibble(Date = x$TimePeriod$Start,
-           Service = x$Groups |> map(function(y){y$Keys}) |> map_chr(~.x[1]),
-           Linked_Account = x$Groups |> map(function(y){y$Keys}) |> map_chr(~.x[2]),
-           Unblended_Cost = x$Groups |> map(function(y){y$Metrics$UnblendedCost$Amount}) |> unlist() |> as.double())
-  }) |>
-  list_rbind()
+# function factory to create functions for both blended and unblended data
+billing_factory <- function(type) {
+  function(date_start, date_end) {
+    ce <- paws::costexplorer()
+    raw_billing_data <- ce$get_cost_and_usage(
+      TimePeriod = list(Start = date_start, End = date_end),
+      Granularity = "DAILY",
+      Metrics = type,
+      GroupBy = list(
+        list(Type = "DIMENSION", Key = "SERVICE"),
+        list(Type = "DIMENSION", Key = "LINKED_ACCOUNT")
+      )
+    )
 
-raw_billing_data <- ce$get_cost_and_usage(
-  TimePeriod = list(Start = "2023-01-01", End = as.character(Sys.Date())),
-  Granularity = "DAILY",
-  Metrics = "BlendedCost",
-  GroupBy = list(
-    list(Type = "DIMENSION", Key = "SERVICE"),
-    list(Type = "DIMENSION", Key = "LINKED_ACCOUNT")
-  )
-)
+    raw_billing_data$ResultsByTime |>
+      map(function(x){
+        tibble(Date = x$TimePeriod$Start,
+               Service = x$Groups |> map(function(y){y$Keys}) |> map_chr(~.x[1]),
+               Linked_Account = x$Groups |> map(function(y){y$Keys}) |> map_chr(~.x[2]),
+               "{type}" := x$Groups |>
+                map(function(y){y$Metrics[[type]]$Amount}) |>
+                unlist() |>
+                as.double())
+      }) |>
+      list_rbind()
+  }
+}
 
-billing_data <- raw_billing_data$ResultsByTime |>
-  map(function(x){
-    tibble(Date = x$TimePeriod$Start,
-           Service = x$Groups |> map(function(y){y$Keys}) |> map_chr(~.x[1]),
-           Linked_Account = x$Groups |> map(function(y){y$Keys}) |> map_chr(~.x[2]),
-           Blended_Cost = x$Groups |> map(function(y){y$Metrics$BlendedCost$Amount}) |> unlist() |> as.double())
-  }) |>
-  list_rbind()
-
-ubilling_data$Unblended_Cost |> sum()
-billing_data$Blended_Cost |> sum()
+billing_unblended <- billing_factory("UnblendedCost")
+billing_blended <- billing_factory("BlendedCost")
--- R/bucket.R
+++ R/bucket.R
@@ -1,65 +1,62 @@
-library(tidyverse)
-library(paws)
+## TODO: not looked over this file yet (commented out below so package can run)
 
-sss <- paws::s3()
+# sss <- paws::s3()
 
-sss$create_bucket(Bucket = "s64-test-3-private",
-                  CreateBucketConfiguration = list(LocationConstraint = "us-west-2"))
+# sss$create_bucket(Bucket = "s64-test-3-private",
+#                   CreateBucketConfiguration = list(LocationConstraint = "us-west-2"))
 
-sss$put_public_access_block(Bucket = "s64-test-2",
-                            PublicAccessBlockConfiguration = list(
-                              BlockPublicPolicy = FALSE,
-                              RestrictPublicBuckets = FALSE
-                            ))
+# sss$put_public_access_block(Bucket = "s64-test-2",
+#                             PublicAccessBlockConfiguration = list(
+#                               BlockPublicPolicy = FALSE,
+#                               RestrictPublicBuckets = FALSE
+#                             ))
 
-sss$put_object(
-  Body = "~/Desktop/koth.png",
-  Bucket = "s64-test-2",
-  Key = basename("~/Desktop/koth.png"),
-  Tagging = NULL)
+# sss$put_object(
+#   Body = "~/Desktop/koth.png",
+#   Bucket = "s64-test-2",
+#   Key = basename("~/Desktop/koth.png"),
+#   Tagging = NULL)
 
-sss$put_object_acl(
-  AccessControlPolicy = structure(
-    list(),
-    names = character(
-      0
-    )
-  ),
-  Bucket = "s64-test-2",
-  Key = basename("~/Desktop/koth.png"),
-  GrantRead = "uri=http://acs.amazonaws.com/groups/global/AllUsers"
-)
+# sss$put_object_acl(
+#   AccessControlPolicy = structure(
+#     list(),
+#     names = character(
+#       0
+#     )
+#   ),
+#   Bucket = "s64-test-2",
+#   Key = basename("~/Desktop/koth.png"),
+#   GrantRead = "uri=http://acs.amazonaws.com/groups/global/AllUsers"
+# )
 
-sss$list_objects("s64-test-2")
+# sss$list_objects("s64-test-2")
 
-policy <- '{
-  "Version":"2012-10-17",
-  "Statement":[{
-    "Sid":"PublicReadGetObject",
-    "Effect":"Allow",
-    "Principal": "*",
-    "Action":"s3:GetObject",
-    "Resource":"arn:aws:s3:::s64-test-2/*"
-  }]
-}'
+# policy <- '{
+#   "Version":"2012-10-17",
+#   "Statement":[{
+#     "Sid":"PublicReadGetObject",
+#     "Effect":"Allow",
+#     "Principal": "*",
+#     "Action":"s3:GetObject",
+#     "Resource":"arn:aws:s3:::s64-test-2/*"
+#   }]
+# }'
 
-sss$put_bucket_policy(Bucket = "s64-test-2", Policy = policy)
+# sss$put_bucket_policy(Bucket = "s64-test-2", Policy = policy)
 
-sss$object
+# sss$object
 
-buckets <- sss$list_buckets()
-buckets[[1]] |>
-  map_chr(~ .x$Name)
+# buckets <- sss$list_buckets()
+# buckets[[1]] |>
+#   map_chr(~ .x$Name)
 
-sss$put_bucket_ownership_controls(Bucket = "s64-test-2",
-                                  OwnershipControls = list(
-                                    Rules = list(
-                                      list(
-                                        ObjectOwnership = "ObjectWriter"
-                                      )
-                                    )
-                                  ))
+# sss$put_bucket_ownership_controls(Bucket = "s64-test-2",
+#                                   OwnershipControls = list(
+#                                     Rules = list(
+#                                       list(
+#                                         ObjectOwnership = "ObjectWriter"
+#                                       )
+#                                     )
+#                                   ))
 
-sss$get_bucket_ownership_controls(Bucket = "s64-test-2")
-
-
+# sss$get_bucket_ownership_controls(Bucket = "s64-test-2")
