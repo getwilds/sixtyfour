8365133360d93dbc127278c6b52050615d4d0a36
Scott Chamberlain <sachamber@fredhutch.org>
2024-03-07 10:01:29 -0800

    random user fxn to be used in aws_db_rds_create

---
+++ tests/testthat/test-random_user.R
@@ -0,0 +1,11 @@
+test_that("random_user", {
+  # character
+  expect_type(random_user(), "character")
+  # non-empty
+  expect_true(nzchar(random_user()))
+  # all no longer than 16 length
+  for (i in replicate(100, random_user())) {
+    # print(nchar(i))
+    expect_true(nchar(i) <= 16)
+  }
+})
