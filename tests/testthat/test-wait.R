f3e833f252fad90062adc0a26567e9e7e207c9cb
Scott Chamberlain <sachamber@fredhutch.org>
2024-04-08 16:03:10 -0700

    added some tests for wait fxns

---
+++ tests/testthat/test-wait.R
@@ -0,0 +1,16 @@
+test_that("wait", {
+  expect_type(wait_until(), "closure")
+
+  my_fun <- function(...) {
+    "available"
+  }
+
+  wait_for_test <- wait_until(
+    my_fun,
+    "test initializing"
+  )
+
+  expect_message((out <- wait_for_test()),
+    "Waiting for")
+  expect_null(out)
+})
