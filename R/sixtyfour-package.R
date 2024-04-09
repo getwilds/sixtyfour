4ab338310efbff2b4cf3b4f98a0c445814c461e5
Scott Chamberlain <sachamber@fredhutch.org>
2024-04-09 11:40:57 -0700

    remove pkg level man file text - use autogenerated

--- R/sixtyfour-package.R
+++ R/sixtyfour-package.R
@@ -1,9 +1,3 @@
-#' sixtyfour: Humane Interface to AWS
-#'
-#' Write some more things here to give folks a
-#' sense of what the goal of this package is,
-#' high level entrypoints, etc.
-#'
 #' @keywords internal
 "_PACKAGE"
 
