0b8cd824e014a178d79d684e1d4daa4d7b7837a3
Scott Chamberlain <sachamber@fredhutch.org>
2025-02-01 07:20:21 -0800

    woopsy, library call was not in examples block in aws_file_attr

--- R/files.R
+++ R/files.R
@@ -276,8 +276,8 @@ aws_file_delete_one <- function(one_path, ...) {
 #' of `remote_path`
 #' @note uses [s3fs::s3_file_info()] internally
 #' @family files
-#' library(glue)
 #' @examplesIf aws_has_creds()
+#' library(glue)
 #' bucket <- random_string("bucket")
 #' aws_bucket_create(bucket)
 #'
